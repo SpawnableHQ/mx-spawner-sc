74f41b097a832ca6338a43d52e73dc4a11aaaa6e
micha vie <micha@freeiam.com>
2023-08-11 15:15:47 +0200

    add contract spawn endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,31 @@ pub trait MinterContract {
     #[init]
     fn init(&self) {}
 
+    #[endpoint(spawnContract)]
+    fn spawn_contract_endpoint(
+        &self,
+        code: ManagedBuffer,
+        code_metadata: CodeMetadata,
+        gas: u64,
+        args: MultiValueEncoded<ManagedBuffer>,
+    ) {
+        let (address, _) = self.send_raw().deploy_contract(
+            gas,
+            &BigUint::zero(),
+            &code,
+            code_metadata,
+            &args.to_arg_buffer(),
+        );
+
+        self.contracts().insert(address);
+    }
+
     #[endpoint(spawnObject)]
     fn spawn_object_endpoint(&self) {
         //
     }
+
+    #[view(getContracts)]
+    #[storage_mapper("contracts")]
+    fn contracts(&self) -> UnorderedSetMapper<ManagedAddress>;
 }
